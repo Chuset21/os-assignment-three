41d27043d5b30f6eff2e56a028f207791e29ee21
Miguel Bloggs <arrietam@tcd.ie>
2022-11-05 20:23:22 -0400

    Initial commit

---
+++ sfs_api.c
@@ -0,0 +1,40 @@
+#include "sfs_api.h"
+#include "disk_emu.h"
+
+
+void mksfs(int x) {
+
+}
+
+int sfs_getnextfilename(char *x) {
+    return -1;
+}
+
+int sfs_getfilesize(const char *x) {
+    return -1;
+}
+
+int sfs_fopen(char *x) {
+    return -1;
+}
+
+int sfs_fclose(int x) {
+    return -1;
+}
+
+int sfs_fwrite(int x, const char *y, int z) {
+    return -1;
+}
+
+int sfs_fread(int x, char *y, int z) {
+    return -1;
+}
+
+int sfs_fseek(int x, int y) {
+    return -1;
+}
+
+int sfs_remove(char *x) {
+    return -1;
+}
+
