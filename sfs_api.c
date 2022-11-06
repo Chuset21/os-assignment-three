b56e87e10242c8c15c16665867d45ce34c531822
Miguel Bloggs <arrietam@tcd.ie>
2022-11-06 12:12:22 -0500

    Changed parameter names

--- sfs_api.c
+++ sfs_api.c
@@ -2,39 +2,39 @@
 #include "disk_emu.h"
 
 
-void mksfs(int x) {
+void mksfs(int fresh) {
 
 }
 
-int sfs_getnextfilename(char *x) {
+int sfs_getnextfilename(char *file_name) {
     return -1;
 }
 
-int sfs_getfilesize(const char *x) {
+int sfs_getfilesize(const char *path) {
     return -1;
 }
 
-int sfs_fopen(char *x) {
+int sfs_fopen(char *file_name) {
     return -1;
 }
 
-int sfs_fclose(int x) {
+int sfs_fclose(int fileID) {
     return -1;
 }
 
-int sfs_fwrite(int x, const char *y, int z) {
+int sfs_fwrite(int fileID, const char *buf, int length) {
     return -1;
 }
 
-int sfs_fread(int x, char *y, int z) {
+int sfs_fread(int fileID, char *buf, int length)  {
     return -1;
 }
 
-int sfs_fseek(int x, int y) {
+int sfs_fseek(int fileID, int location) {
     return -1;
 }
 
-int sfs_remove(char *x) {
+int sfs_remove(char *file_name) {
     return -1;
 }
 
